63da7101b98c14cfbd69b698a64c3226c8f68ada
jindongdu <djd28176@hotmail.com>
2023-12-11 10:43:40 -0500

    Initial Commit

---
+++ pokemonApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pokemonApp
+//
+//  Created by Jindong Du on 12/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pokemonApp/pokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemonAppApp.swift
+//  pokemonApp
+//
+//  Created by Jindong Du on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct pokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
